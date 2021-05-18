8b5a43795d8c634a727990e69c35c08928561bec
jmsilvas <jmsilva10@uc.cl>
2021-05-18 13:30:07 -0400

    position and routes displaying, fixes required for changing colors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
 import './App.css';
+import React, {useEffect, useState} from "react";
 import Map from './components/Map';
 import {Container, Row, Col} from 'reactstrap';
+import { io } from "socket.io-client";
+
 
 function App() {
+  const [flights, setFlights] = useState([]);
+  const [positions, setPosition] = useState({});
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
+
+  useEffect(() => {
+    
+
+    const socket = io("wss://tarea-3-websocket.2021-1.tallerdeintegracion.cl", {
+      path: "/flights"});
+      socket.emit("FLIGHTS")
+      // socket.onAny((event, ...args) => {
+      //   console.log(`got ${event}`);
+      // });
+      socket.on("FLIGHTS", (payload) => {
+        for (let index = 0; index < payload.length; index++) {
+          payload[index]["color"]=getRandomColor();
+          
+        }
+        setFlights(payload)
+});
+      socket.on("POSITION", (payload) => {
+        positions[payload.code] = payload.position
+        setPosition(positions)
+      });
+    },[flights, positions])
+  
   return (
     <Container className="main-container">
       <Row>
         <Col md="8" sm="12">
-          <Map/>
+          <Map flights={flights} positions={positions}/>
         </Col>
         <Col md="4" sm="12">Chat</Col>
       </Row>
-      <Row>
-        <Col md="12">Info</Col>
+      <Row md="12">
+          <h2>Info</h2>
+          {flights.length ? flights.map((flight) => (
+          <Col key={flight.code}>
+            <div>{flight.code}</div>
+            <div>{flight.airline}</div>
+            {/* <div>[{flight.origin}]</div>
+            <div>[{flight.destination}]</div> */}
+            <div>{flight.plane}</div>
+            <div>{flight.seats}</div>
+            {/* <div>{flight.passengers[0].name}</div>    */}
+          </Col>)) : ""}
       </Row>
 
     </Container>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,43 @@
 import React from 'react';
-import { MapContainer, TileLayer} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Polyline} from 'react-leaflet';
 // import "leaflet/dist/leaflet.css"
 
-export default function Map() {
+export default function Map(props) {
+    
+  
     return (
         <div>
             <div id="mapid">
-                <MapContainer center={[25.505, -0.09]} zoom={1} scrollWheelZoom={false}>
+                <MapContainer center={[-38.505, -65.09]} zoom={5} scrollWheelZoom={false}>
                     <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    {/* <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
+                    {props.flights.map((flight) => ( 
+                        <>
+                    {/* <Marker key={flight.code+"ori"} position={[flight.origin[0], flight.origin[1]]}>
+                        <Popup>
+                            Origen Vuelo {flight.code}
+                            {props.positions[flight.code]? props.positions[flight.code]: "no hay avance"}
+                        </Popup>
+                    </Marker> 
+                    <Marker key={flight.code+"dest"} position={[flight.destination[0], flight.destination[1]]}>
+                        <Popup>
+                            Destino Vuelo {flight.code}
+                            {props.positions[flight.code]? props.positions[flight.code].position: "no hay avance"}
+                        </Popup>
                     </Marker>  */}
+                    <Polyline pathOptions={{color: flight.color}} positions={[flight.origin, flight.destination]}>
+                        <Popup>{flight.code}</Popup>
+                    </Polyline>
+                    {props.positions[flight.code]&&   <Marker key={flight.code+"loc"} position={[props.positions[flight.code][0],props.positions[flight.code][1]]}>
+                        <Popup>
+                            Posicion actual Vuelo {flight.code}
+                            {props.positions[flight.code]? props.positions[flight.code].position: "no hay avance"}
+                        </Popup>
+                    </Marker> }
+                    </>))}
+                   
                 </MapContainer>
             </div>
         </div>
