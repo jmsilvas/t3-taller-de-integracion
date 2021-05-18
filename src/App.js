ab5e2f5c9bfe8c5bc8764bab360e3dac7aa61882
jmsilvas <jmsilva10@uc.cl>
2021-05-17 21:46:34 -0400

    grid layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import Map from './components/Map';
+import {Container, Row, Col} from 'reactstrap';
 
 function App() {
   return (
-    <Map/>
+    <Container className="main-container">
+      <Row>
+        <Col md="8" sm="12">
+          <Map/>
+        </Col>
+        <Col md="4" sm="12">Chat</Col>
+      </Row>
+      <Row>
+        <Col md="12">Info</Col>
+      </Row>
+
+    </Container>
   );
 }
 
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer} from 'react-leaflet';
 // import "leaflet/dist/leaflet.css"
 
 export default function Map() {
     return (
         <div>
             <div id="mapid">
-                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <MapContainer center={[25.505, -0.09]} zoom={1} scrollWheelZoom={false}>
                     <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={[51.505, -0.09]}>
+                    {/* <Marker position={[51.505, -0.09]}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
-                    </Marker> 
+                    </Marker>  */}
                 </MapContainer>
             </div>
         </div>
